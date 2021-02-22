2e24c7e394830042afece01f7c41d4c81cb586d5
Arnav1439 <arnav7084@gmail.com>
2021-02-22 15:57:32 +0530

    added runbber band and catapault

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.sling1=loadImage("sprites/sling1.png")
+        this.sling2=loadImage("sprites/sling2.png")
+        this.sling3=loadImage("sprites/sling3.png")
         World.add(world, this.sling);
     }
 
@@ -17,10 +20,27 @@ class SlingShot{
 
     display(){
         if(this.sling.bodyA){
+            //wooden slingshot
+            image(this.sling2,170,20);
+            image(this.sling1,200,20);
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push()
+            if(pointA.x<220){
+                strokeWeight(7);
+                stroke(48,22,8)
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+            }else{
+                strokeWeight(4);
+                stroke(48,22,8)
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+            }
+           
+           
+            image(this.sling3,pointA.x-30,pointA.y-10,15,30);
+            pop()
         }
     }
     
